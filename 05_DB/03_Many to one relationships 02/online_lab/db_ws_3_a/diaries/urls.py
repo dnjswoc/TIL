50831ee3be843317753f0ae610a5b2087e0b8093
dnjswoc <dnjswoc@gmail.com>
2024-10-20 23:32:37 +0900

    db online lab upload

---
+++ 05_DB/03_Many to one relationships 02/online_lab/db_ws_3_a/diaries/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'diaries'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('<int:pk>/create/comment/', views.create_comment, name='create_comment'),
+]
