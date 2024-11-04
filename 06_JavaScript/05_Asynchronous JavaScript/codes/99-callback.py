b66777d2004d349d9a57ef70c637bba7078675a9
dnjswoc <dnjswoc@gmail.com>
2024-11-04 23:47:04 +0900

    asynchronous js codes update

--- 06_JavaScript/05_Asynchronous JavaScript/codes/99-callback.py
+++
@@ -1,9 +0,0 @@
-numbers = [1, 2, 3]
-
-
-def square(num):
-    return num**2
-
-
-new_numbers = list(map(square, numbers))
-print(new_numbers)  # [1, 4, 9]
---
+++ 06_JavaScript/05_Asynchronous JavaScript/codes/request-in-python.py
@@ -0,0 +1,5 @@
+import requests
+
+
+response = requests.get('서버 url')
+data = response.json()
