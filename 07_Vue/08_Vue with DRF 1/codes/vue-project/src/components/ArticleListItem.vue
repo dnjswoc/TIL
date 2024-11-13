17ed6fa4fc457ab9610bf0151bdb4f768eeddb17
dnjswoc <dnjswoc@gmail.com>
2024-11-13 15:59:07 +0900

    vue with drf1 codes upload

---
+++ 07_Vue/08_Vue with DRF 1/codes/vue-project/src/components/ArticleListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h5>{{ article.id }}</h5>
+    <p>{{ article.title }}</p>
+    <p>{{ article.content }}</p>
+    <hr>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  article: Object
+})
+
+</script>
---
+++ 07_Vue/08_Vue with DRF 1/codes/vue-project/src/views/ArticleView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Article Page</h1>
+    <ArticleList />
+  </div>
+</template>
+
+<script setup>
+import ArticleList from '@/components/ArticleList.vue'
+import { onMounted } from 'vue'
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+
+onMounted(() => {
+  // mount 되기 전에 store에 있는 전체 게시글 요청 함수를 호출
+  store.getArticles()
+})
+
+</script>
+
+<style>
+
+</style>
